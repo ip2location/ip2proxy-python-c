f75e49d582d16c9049de79a9f06aa07debf13dd5
IP2Location <support@ip2location.com>
2022-02-24 09:30:41 +0800

    Minor updates
    - Fixed Python 3 bytes issue.
    - Added support for threat and provider information.

--- ip2proxy_python_c.py
+++ ip2proxy_python_c.py
@@ -16,15 +16,31 @@
 
 from ctypes import *
 from ctypes.util import find_library
+import sys
 
-_VERSION = '2.0.0'
+_VERSION = '2.1.0'
 _INVALID_IP_ADDRESS  = 'INVALID IP ADDRESS'
+
+if sys.version < '3':
+    def u(x):
+        return x.decode('utf-8')
+    def b(x):
+        return str(x)
+else:
+    def u(x):
+        if isinstance(x, bytes):
+            return x.decode()
+        return x
+    def b(x):
+        if isinstance(x, bytes):
+            return x
+        return x.encode('ascii')
 
 class C_IP2ProxyRecord(Structure):
     '''
         Define the IP2Location Record result structure.
     '''
-    _fields_=[("country_short",c_char_p),("country_long",c_char_p),("region",c_char_p),("city",c_char_p),("isp",c_char_p),("is_proxy",c_char_p),("proxy_type",c_char_p),("domain",c_char_p),("usage_type",c_char_p),("asn",c_char_p),("as_",c_char_p),("last_seen",c_char_p)]
+    _fields_=[("country_short",c_char_p),("country_long",c_char_p),("region",c_char_p),("city",c_char_p),("isp",c_char_p),("is_proxy",c_char_p),("proxy_type",c_char_p),("domain",c_char_p),("usage_type",c_char_p),("asn",c_char_p),("as_",c_char_p),("last_seen",c_char_p),("threat",c_char_p),("provider",c_char_p)]
 
 class IP2Proxy(object):
     def __init__(self, filename=None, libraryname = None):
@@ -40,13 +56,13 @@ class IP2Proxy(object):
         '''
             Function to load the IP2Location C Library if user choose to load their own copy of IP2Location C Library.
         '''
-        self.ip2proxy_c = CDLL(libraryname)
+        self.ip2proxy_c = CDLL(b(libraryname))
 
     def open(self, filename):
         ''' Opens a database file '''
         self.ip2proxy_c.IP2Proxy_open.argtypes = [c_char_p]
         self.ip2proxy_c.IP2Proxy_open.restype = c_void_p
-        self.ip2proxy_c_pointer = self.ip2proxy_c.IP2Proxy_open(filename)
+        self.ip2proxy_c_pointer = self.ip2proxy_c.IP2Proxy_open(b(filename))
 
     def get_module_version(self):
         return _VERSION
@@ -55,21 +71,21 @@ class IP2Proxy(object):
         self.ip2proxy_c.IP2Proxy_get_package_version.argtypes = [c_void_p]
         self.ip2proxy_c.IP2Proxy_get_package_version.restype = c_char_p
         ip2proxy_package_version = self.ip2proxy_c.IP2Proxy_get_package_version(self.ip2proxy_c_pointer)
-        return str(ip2proxy_package_version)
+        return u(ip2proxy_package_version)
 
     def get_database_version(self) :
         self.ip2proxy_c.IP2Proxy_get_database_version.argtypes = [c_void_p]
         self.ip2proxy_c.IP2Proxy_get_database_version.restype = c_char_p
         ip2proxy_database_version = self.ip2proxy_c.IP2Proxy_get_database_version(self.ip2proxy_c_pointer)
-        return str(ip2proxy_database_version)
+        return u(ip2proxy_database_version)
 
     def get_all(self, ip):
         ''' Get the whole record with all fields read from the file '''
-        ''' set the argument and response types of the function for data compatibility issue. '''
+        ''' Set the argument and response types of the function for data compatibility issue. '''
         self.ip2proxy_c.IP2Proxy_get_all.argtypes = [c_void_p, c_char_p]
         self.ip2proxy_c.IP2Proxy_get_all.restype = POINTER(C_IP2ProxyRecord)
         try:
-            self.rec = self.ip2proxy_c.IP2Proxy_get_all(self.ip2proxy_c_pointer, ip)
+            self.rec = self.ip2proxy_c.IP2Proxy_get_all(self.ip2proxy_c_pointer, b(ip))
             country_short = self.rec.contents.country_short
             country_long = self.rec.contents.country_long
             region = self.rec.contents.region
@@ -82,6 +98,8 @@ class IP2Proxy(object):
             asn = self.rec.contents.asn
             as_name = self.rec.contents.as_
             last_seen = self.rec.contents.last_seen
+            threat = self.rec.contents.threat
+            provider = self.rec.contents.provider
         except:
             country_short = _INVALID_IP_ADDRESS
             country_long = _INVALID_IP_ADDRESS
@@ -94,81 +112,85 @@ class IP2Proxy(object):
             asn = _INVALID_IP_ADDRESS
             as_name = _INVALID_IP_ADDRESS
             last_seen = _INVALID_IP_ADDRESS
+            threat = _INVALID_IP_ADDRESS
+            provider = _INVALID_IP_ADDRESS
             is_proxy = -1
 
         results = {}
-        results['is_proxy'] = is_proxy
-        results['proxy_type'] = proxy_type
-        results['country_short'] = country_short
-        results['country_long'] = country_long
-        results['region'] = region
-        results['city'] = city
-        results['isp'] = isp
-        results['domain'] = domain
-        results['usage_type'] = usage_type
-        results['asn'] = asn
-        results['as_name'] = as_name
-        results['last_seen'] = last_seen
+        results['is_proxy'] = u(is_proxy)
+        results['proxy_type'] = u(proxy_type)
+        results['country_short'] = u(country_short)
+        results['country_long'] = u(country_long)
+        results['region'] = u(region)
+        results['city'] = u(city)
+        results['isp'] = u(isp)
+        results['domain'] = u(domain)
+        results['usage_type'] = u(usage_type)
+        results['asn'] = u(asn)
+        results['as_name'] = u(as_name)
+        results['last_seen'] = u(last_seen)
+        results['threat'] = u(threat)
+        results['provider'] = u(provider)
         return results
 
     def get_country_short(self, ip):
         ''' Get country_short '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             country_short = record['country_short']
         except:
             country_short = _INVALID_IP_ADDRESS
-        return country_short
+        return u(country_short)
 
     def get_country_long(self, ip):
         ''' Get country_long '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             country_long = record['country_long']
         except:
             country_long = _INVALID_IP_ADDRESS
-        return country_long
+        return u(country_long)
 
     def get_region(self, ip):
         ''' Get region '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             region = record['region']
         except:
             region = _INVALID_IP_ADDRESS
-        return region
+        return u(region)
 
     def get_city(self, ip):
         ''' Get city '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             city = record['city']
         except:
             city = _INVALID_IP_ADDRESS
-        return city
+        return u(city)
 
     def get_isp(self, ip):
         ''' Get isp '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             isp = record['isp']
         except:
             isp = _INVALID_IP_ADDRESS
-        return isp
+        return u(isp)
 
     def get_proxy_type(self, ip):
         ''' Get proxy_type '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             proxy_type = record['proxy_type']
         except:
             proxy_type = _INVALID_IP_ADDRESS
-        return proxy_type
+        return u(proxy_type)
 
     def is_proxy(self, ip):
         ''' Determine whether is a proxy '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             is_proxy = record['is_proxy']
         except:
             is_proxy = -1
@@ -177,51 +199,65 @@ class IP2Proxy(object):
     def get_domain(self, ip):
         ''' Get domain '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             domain = record['domain']
         except:
             domain = _INVALID_IP_ADDRESS
-        return domain
-
+        return u(domain)
 
     def get_usage_type(self, ip):
         ''' Get usage_type '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             usage_type = record['usage_type']
         except:
             usage_type = _INVALID_IP_ADDRESS
-        return usage_type
-
+        return u(usage_type)
 
     def get_asn(self, ip):
         ''' Get asn '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             asn = record['asn']
         except:
             asn = _INVALID_IP_ADDRESS
-        return asn
-
+        return u(asn)
 
     def get_as_name(self, ip):
         ''' Get as_name '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             as_name = record['as_name']
         except:
             as_name = _INVALID_IP_ADDRESS
-        return as_name
-
+        return u(as_name)
 
     def get_last_seen(self, ip):
         ''' Get last_seen '''
         try:
-            record = self.get_all(ip)
+            record = self.get_all(b(ip))
             last_seen = record['last_seen']
         except:
             last_seen = _INVALID_IP_ADDRESS
-        return last_seen
+        return u(last_seen)
+
+    def get_threat(self, ip):
+        ''' Get threat '''
+        try:
+            record = self.get_all(b(ip))
+            threat = record['threat']
+        except:
+            threat = _INVALID_IP_ADDRESS
+        return u(threat)
+
+    def get_provider(self, ip):
+        ''' Get provider '''
+        try:
+            record = self.get_all(b(ip))
+            provider = record['provider']
+        except:
+            provider = _INVALID_IP_ADDRESS
+        return u(provider)
 
     def close(self):
         self.ip2proxy_c.IP2Proxy_close.argtypes = [c_void_p]
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="ip2proxy_python_c",
-	version="2.0.0", 
+	version="2.1.0", 
 	author="IP2Location",
 	author_email="support@ip2location.com",
 	description="IP2Proxy Python library with C module to query an IP address if it was being used as open proxy, web proxy, VPN anonymizer and TOR exits from IP2Proxy database",
@@ -17,12 +17,12 @@ setuptools.setup(
 	py_modules=['ip2proxy_python_c'],
 	url="https://github.com/ip2location/ip2proxy-python",
 	packages=setuptools.find_packages(),
-	classifiers=(
+	classifiers=[
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
 		"Topic :: Software Development :: Libraries :: Python Modules",
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
-	),
+	],
 )
